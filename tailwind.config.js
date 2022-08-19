36bcbca451da22acc7c6da9891d6b538e625512c
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2022-08-20 01:54:24 +0430

    import Vazirmatn font and add to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
+		fontFamily: {
+			vazirmatn: ['Vazirmatn'],
+			vazirmatnFD: ['Vazirmatn FD']
+		},
 		extend: {},
 	},
 	plugins: [],
